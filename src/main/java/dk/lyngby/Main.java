b62599ffd956ea2bfb182115e39ced44b172f6f5
Jörg Oertel <joergoertel@proton.me>
2023-06-11 10:20:31 +0200

    first commit

---
+++ src/main/java/dk/lyngby/Main.java
@@ -0,0 +1,12 @@
+package dk.lyngby;
+
+import io.javalin.Javalin;
+
+public class Main {
+    public static void main(String[] args) {
+        var app = Javalin.create(/*config*/)
+                .get("/", ctx -> ctx.result("Hello World"))
+                .start(7070);
+    }
+
+}
