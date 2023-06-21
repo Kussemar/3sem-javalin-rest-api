646a565d4a9164f36e47c2d84dc9f9ad924469a7
Jörg Oertel <joergoertel@proton.me>
2023-06-21 17:17:10 +0200

    refactored

--- src/main/java/dk/lyngby/Main.java
+++ src/main/java/dk/lyngby/Main.java
@@ -5,7 +5,7 @@ import io.javalin.Javalin;
 public class Main {
     public static void main(String[] args) {
         var app = Javalin.create(/*config*/)
-                .get("/", ctx -> ctx.result("Hello World And Welcome to the world of Javalin!"))
+                .get("/", ctx -> ctx.result("Welcome to the world of Javalin!"))
                 .start(7070);
     }
 
